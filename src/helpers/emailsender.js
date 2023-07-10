19a33db012c3607db3323ff25b9b81752f9378d3
Nitesh Kumar <niteshstudy007@gmail.com>
2023-07-10 16:13:15 +0530

    fixed email feature

--- src/helpers/emailsender.js
+++ src/helpers/emailsender.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 import UserModeler from '@/models/user';
 import bcryptjs from 'bcryptjs'
 import mongodbConnector from '@/dbConfig/mongoConn.ts';
-async function x(){
+async function x() {
     await mongodbConnector()
 }
 x()
@@ -10,35 +10,41 @@ x()
 export default async function sendEmail(email, emailType, userKey) {
     try {
         const hashedToken = await bcryptjs.hash('dekh', 10)
-        console.log('userKey : ',userKey)
-        console.log('hashedToken : ',hashedToken)
+        console.log('userKey : ', userKey)
+        console.log('hashedToken : ', hashedToken)
         const options = { maxTimeMS: 60000 }
         switch (emailType) {
             case 'VerifyEmail':
-        
-                const expiryDuration = 60*60*1000 //milliseconds in 1hr
-                await UserModeler.findOneAndUpdate({_id:userKey.toString()}, { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + expiryDuration },options)
+
+                const expiryDuration = 2 * 60 * 60 * 1000 //milliseconds in 2hr
+                await UserModeler.findOneAndUpdate({ _id: userKey.toString() }, { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + expiryDuration }, options)
                 break
             case 'ResetPass':
-                await await UserModeler.findOneAndUpdate({email:userKey.toString()}, { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: parseInt(Date.now()) + parseInt(36000) },options)
+                await await UserModeler.findOneAndUpdate({ email: userKey.toString() }, { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: parseInt(Date.now()) + parseInt(36000) }, options)
                 break
             default:
                 break
         }
-        var transporter = nodemailer.createTransport({
-            host: "sandbox.smtp.mailtrap.io",
-            port: 2525,
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
             auth: {
-                user: "492b10ca63ac33",
-                pass: "8d0a21f34a021e"
+                user: process.env.ADMIN_EMAIL,
+                pass: process.env.NODEMAILER_PASSKEY
             }
         });
-        const mailOptions =  {
+        //
+        const mailtype = (emailType === 'VerifyEmail') ? 'verifyemail' : 'resetpassword'
+        const isVerifyMail = (mailtype === 'verifymail') ? true : false
+        const task = isVerifyMail ? 'verify your email.' : 'reset the password.'
+        const hrefUrl = `${process.env.DOMAIN}/${mailtype}?token=${hashedToken}`
+
+        const mailOptions = {
             from: 'projectsbyme1234@gmail.com', // sender address
             to: email, // list of receivers
             subject: emailType === 'VerifyEmail' ? 'Email Verification' : 'Reset Password', // Subject line
             text: "Either Copy below link and paste in the required place or click on the link", // plain text body
-            html: `<a href='${process.env.DOMAIN}/${(emailType === 'VerifyEmail') ? 'verifyemail' : 'resetpassword'}?token=${hashedToken}>Click here or Copy this link</a> to  or ${(emailType === 'VerifyEmail') ? 'verify your email.' : 'reset the password.'}`, // html body
+            html: `<a href=${hrefUrl}>Click here or Copy this link</a> to ${task}`, // html body
         }
         const mailResponse = await transporter.sendMail(mailOptions)
         return mailResponse
